71c8a079abb2543286ac2aaa4611e6875574232e
salusn <salu.s@lightrains.com>
2017-04-03 11:58:37 +0530

    Added options slecting action

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import autoBind from 'auto-bind'
-import logo from './logo.svg';
 import './App.css';
 import Test from './components/Test';
+import '../semantic/dist/semantic.min.css';
+import { Container, Header } from 'semantic-ui-react'
+
 
 const jsonquestions = require('./questions/questions.json')
 
@@ -12,10 +14,14 @@ class App extends Component {
     super(props);
     autoBind(this)
 
+    this.getComponent = this.getComponent.bind(this);
+
     this.state = {
       questions : jsonquestions.questions,
       status: false,
-      questionNumber: 0
+      questionNumber: 0,
+      answered: false,
+      answer : ''
     }
     //const ques = this.state.questions[0];
   }
@@ -26,16 +32,48 @@ class App extends Component {
    })
   }
 
+  onClickHandler (index){
+    const currentQuestion = this.state.questions[this.state.questionNumber]
+    const {questions,optiona,optionb,optionc,optiond,answerkey} = this.state.questions[this.state.questionNumber];
+    const options = [optiona,optionb,optionc,optiond]
+    console.log(options[answerkey]);
+    if (currentQuestion.answerkey == index) { //true
+      this.setState({
+        answered: true,
+        answer : options[answerkey]        
+      })
+
+    } else {
+
+    }
+
+   // this.setState({
+   //  answered: true
+   // })
+  }
+
+
+  getComponent(index) {
+   console.log("hh")
+  }
+
   render() {
-    console.log(this.state.questions[0])
+    console.log(this.state.questions[this.state.questionNumber])
+    const {questions,optiona,optionb,optionc,optiond,answerkey} = this.state.questions[this.state.questionNumber];
+    const options = [optiona,optionb,optionc,optiond]
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <Test name="pschub" question={this.state.questions[this.state.questionNumber].questions} />
-      </div>
+      <Container text>
+        <Header as='h2'>Psc Hub</Header>
+        <Test name="pschub" 
+              question={questions} 
+              options={options} 
+              answerkey={answerkey} 
+              getComponent={this.onClickHandler} />
+
+
+        
+      </Container>
     );
   }
 }
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { List, Segment } from 'semantic-ui-react'
+
 
 class Test extends Component {
+
   render() {
+
+  	const {question, options} = this.props
+
     return (
-     <div>
-      <h1>{this.props.question}</h1>
-     </div>
+      
+      <div>
+      <h2>{question}</h2>
+      <Segment inverted >
+          <List divided inverted relaxed>
+            {options.map((option, index) =>
+            <List.Item key={index} onClick={this.props.getComponent.bind(this, index)}>
+              {option}
+            </List.Item>          
+         )}
+          </List>
+          
+        </Segment>
+      </div>     
     );
   }
 }
